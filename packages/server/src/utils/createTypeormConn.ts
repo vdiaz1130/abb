548cf2def255cad0dfa2928c3dcee4cafbeb1491
Victor Diaz <victor.diaz@agilethought.com>
2018-11-21 12:38:37 -0500

    In the middle of dokku auto deploy. Moving to Mac.

--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -1,6 +1,14 @@
-import { getConnectionOptions, createConnection } from "typeorm";
+import { getConnectionOptions, createConnection } from 'typeorm';
+import { User } from '../entity/User';
 
 export const createTypeormConn = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({ ...connectionOptions, name: "default" });
+	const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+	return process.env.NODE_ENV === 'production'
+		? createConnection({
+				...connectionOptions,
+				entities: [ User ],
+				url: process.env.DATABASE_URL,
+				name: 'default'
+			} as any)
+		: createConnection({ ...connectionOptions, name: 'default' });
 };

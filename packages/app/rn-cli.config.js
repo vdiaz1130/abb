c71ff696ad560b395d2df003ba995dd53404e1a8
Victor Diaz <vdiaz1130@gmail.com>
2018-11-22 13:07:44 -0500

    Starting part 16 Adding GraphQL and Routing to React Native

--- packages/app/rn-cli.config.js
+++ packages/app/rn-cli.config.js
@@ -1,6 +1,7 @@
-const getConfig = require("metro-bundler-config-yarn-workspaces");
-const path = require("path");
+const blacklist = require('metro-config/src/defaults/blacklist');
+const getConfig = require('metro-bundler-config-yarn-workspaces');
+const path = require('path');
 
 module.exports = getConfig(__dirname, {
-  nodeModules: path.join(__dirname, "../..")
+  nodeModules: path.join(__dirname, '../..')
 });
